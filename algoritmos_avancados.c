d30e12be71e58145f12416a2a810bb5ae863c5ff
Arlesson Sales <72838594+Arlesson-Sales@users.noreply.github.com>
2025-11-18 14:37:57 -0300

    Update algoritmos_avancados.c

--- algoritmos_avancados.c
+++ algoritmos_avancados.c
@@ -1,47 +1,70 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Desafio Detective Quest
-// Tema 4 - Árvores e Tabela Hash
-// Este código inicial serve como base para o desenvolvimento das estruturas de navegação, pistas e suspeitos.
-// Use as instruções de cada região para desenvolver o sistema completo com árvore binária, árvore de busca e tabela hash.
+#define ROM_NAME_LENGTH 21
 
-int main() {
+/** Representa um node de uma árvore binaria, como também uma sala da mansão. */
+typedef struct Rom
+{
+    char name[ROM_NAME_LENGTH];
+    struct Rom * left;
+    struct Rom * right;
+} Rom;
 
-    // 🌱 Nível Novato: Mapa da Mansão com Árvore Binária
-    //
-    // - Crie uma struct Sala com nome, e dois ponteiros: esquerda e direita.
-    // - Use funções como criarSala(), conectarSalas() e explorarSalas().
-    // - A árvore pode ser fixa: Hall de Entrada, Biblioteca, Cozinha, Sótão etc.
-    // - O jogador deve poder explorar indo à esquerda (e) ou à direita (d).
-    // - Finalize a exploração com uma opção de saída (s).
-    // - Exiba o nome da sala a cada movimento.
-    // - Use recursão ou laços para caminhar pela árvore.
-    // - Nenhuma inserção dinâmica é necessária neste nível.
+/**
+ * @brief Faz a criação de um node que representa uma sala da mensão.
+ * @param name Constante contendo o nome da sala.
+ */
+Rom * createRom(char * name)
+{
+    Rom * rom = (Rom *) malloc(sizeof(Rom));
+    if (rom == NULL) exit(1);
 
-    // 🔍 Nível Aventureiro: Armazenamento de Pistas com Árvore de Busca
-    //
-    // - Crie uma struct Pista com campo texto (string).
-    // - Crie uma árvore binária de busca (BST) para inserir as pistas coletadas.
-    // - Ao visitar salas específicas, adicione pistas automaticamente com inserirBST().
-    // - Implemente uma função para exibir as pistas em ordem alfabética (emOrdem()).
-    // - Utilize alocação dinâmica e comparação de strings (strcmp) para organizar.
-    // - Não precisa remover ou balancear a árvore.
-    // - Use funções para modularizar: inserirPista(), listarPistas().
-    // - A árvore de pistas deve ser exibida quando o jogador quiser revisar evidências.
-
-    // 🧠 Nível Mestre: Relacionamento de Pistas com Suspeitos via Hash
-    //
-    // - Crie uma struct Suspeito contendo nome e lista de pistas associadas.
-    // - Crie uma tabela hash (ex: array de ponteiros para listas encadeadas).
-    // - A chave pode ser o nome do suspeito ou derivada das pistas.
-    // - Implemente uma função inserirHash(pista, suspeito) para registrar relações.
-    // - Crie uma função para mostrar todos os suspeitos e suas respectivas pistas.
-    // - Adicione um contador para saber qual suspeito foi mais citado.
-    // - Exiba ao final o “suspeito mais provável” baseado nas pistas coletadas.
-    // - Para hashing simples, pode usar soma dos valores ASCII do nome ou primeira letra.
-    // - Em caso de colisão, use lista encadeada para tratar.
-    // - Modularize com funções como inicializarHash(), buscarSuspeito(), listarAssociacoes().
-
-    return 0;
+    strncpy(rom->name, name, ROM_NAME_LENGTH - 1);
+    rom->name[ROM_NAME_LENGTH] = '\0';
+    rom->right = NULL;
+    rom->left = NULL;
+    return rom;
 }
 
+/**
+ * @brief Faz um percuso de pós-ordem na árvore binaria que representa a mansão, sempre questionando a próxima direção que o usuário deseja seguir.
+ * @param rom Ponteiro para uma sala.
+ */
+void exploreRom(Rom * rom)
+{
+    if (rom)
+    {
+        char user_option = 's';
+        printf("> voce esta em : %s\n", rom->name);
+        printf("> para qual direcao deseja seguir? (l, r, s): ");
+        scanf("%c", &user_option);
+        getchar();
+    
+        if (user_option == 'l') return exploreRom(rom->left);
+        if (user_option == 'r') return exploreRom(rom->right);
+    
+        printf("\n> voce escolheu sair, ate mais!");
+        return;
+    }
+    printf("> voce chegou a uma sala sem saida!");
+}
+
+/** Fluxo principal do programa. */
+int main(void)
+{
+    //Criando as salas da mansão.
+    Rom * mansion = createRom("Hall de entrada");
+    
+    mansion->left = createRom("Corredor");
+    mansion->left->left = createRom("Quarto 01");
+    mansion->left->right = createRom("Quarto 02");
+
+    mansion->right = createRom("Sala de estar");
+    mansion->right->left = createRom("Jardim");
+    mansion->right->right = createRom("Banheiro");
+
+    exploreRom(mansion);
+    return 0;
+}
